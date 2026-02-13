873adf05b2cacac598765bd5e48758d5e5850598
libai2026 <1026687734@qq.com>
2026-02-13 15:07:18 +0000

    Bypass unstable shell UI flow and jump to OEP after unpack/iat repair

--- CombatShell/CombatShell.cpp
+++ CombatShell/CombatShell.cpp
@@ -739,9 +739,31 @@ void WINAPI CombatShellEntry()
 	MyVirtualProtect = (FnVirtualProtect)puGetProcAddress(g_stud.s_Krenel32, 0xEF64A41E);
 	// GetMyGetProcessAddress
 	MyGetProcAddress = (FnGetProcAddress)puGetProcAddress(g_stud.s_Krenel32, 0xBBAFDF85);
-	
-	SHELL_TRACE("CombatShellEntry:before_CreateWind");
-	CreateWind();
+
+	// Stable shell path: unpack, repair IAT, then jump to original entry point.
+	SHELL_TRACE("CombatShellEntry:before_unpack");
+	UnCompression();
+	SHELL_TRACE("CombatShellEntry:before_iat");
+	RepairTheIAT();
+	SHELL_TRACE("CombatShellEntry:before_oep");
+#ifdef _WIN64
+	CodeExecEntry(g_stud.s_dwOepBase);
+#else
+	__asm {
+		push esi;
+		push eax;
+		mov	 esi, g_stud.s_dwOepBase;
+		xor	 eax, eax;
+		add  eax, 0x200000;
+		add	 eax, 0x200000;
+		add	 eax, 0x200000;
+		sub  eax, 0x200000;
+		add  esi, eax;
+		jmp	 esi;
+		pop eax;
+		pop esi;
+	}
+#endif
 	SHELL_TRACE("CombatShellEntry:return");
 }
 
